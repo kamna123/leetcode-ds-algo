5ed6f946733a7869c192dfc9369eb61007b7fb1c
Kamna Garg <kamna90123@gmail.com>
2023-06-19 13:17:09 +0100

    Time: 12 ms (50.00%), Space: 5.1 MB (100.00%) - LeetHub

---
+++ 0280-wiggle-sort/0280-wiggle-sort.go
@@ -0,0 +1,9 @@
+func wiggleSort(nums []int)  {
+    for i := 0; i < len(nums) - 1; i++ {
+        if (i % 2 == 0 && nums[i] > nums[i + 1]) || (i % 2 == 1 && nums[i] < nums[i + 1]) {
+            temp := nums[i]
+            nums[i] = nums[i + 1]
+            nums[i + 1] = temp;
+        }
+    }
+}
