383b40bee43b3096ea7da5203dfb66ec350d7dc9
Kamna Garg <kamna90123@gmail.com>
2023-07-09 23:41:19 +0100

    Time: 3 ms (94.44%), Space: 7 MB (47.62%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.go
@@ -0,0 +1,11 @@
+func removeDuplicates(s string) string {
+    stack := []rune{}
+	for _, c := range s {
+		if len(stack) > 0 && stack[len(stack)-1] == c {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, c)
+		}
+	}
+	return string(stack)
+}
