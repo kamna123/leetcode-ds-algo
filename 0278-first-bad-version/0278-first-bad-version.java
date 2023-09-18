cf404cddc7517874401eda0bbcdf2d2862649c40
Kamna Garg <kamna90123@gmail.com>
2023-09-18 12:44:39 +0100

    Time: 13 ms (81.50%), Space: 39.2 MB (51.35%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,18 +3,19 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-          int left = 1;
-          int right = n;
-          int badVersion = 1;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (isBadVersion(mid)) {
-                    badVersion = mid;
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+        int left = 1;
+        int right = n;
+        int badVersion = 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                badVersion = mid;
+                right = mid - 1;
+                
+            } else {
+                left = mid + 1;
             }
-            return badVersion;
+        }
+        return badVersion;
     }
 }
